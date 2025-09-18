2057e5b2ed51758e0f2ce994345cd0448f6ecfa0
ayushmanalways <ayushmanalways0001@gmail.com>
2025-09-18 10:25:58 +0000

    comparison done

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -19,3 +19,9 @@ console.log(undefined < 0);
 // === 
 
 console.log("2" === 2);
+
+
+// Stack- yaha pe data ka reference milta hai toh jo original value hoti hai usme koi change nhi hota ha tum reference leke secondary value me change kr skte ho
+
+// Heap- ya pe actual data se reference milta hai yani variable direct data klo pin point karta hai agar ek me bhi change kiye toh dono change ho jayega
+
