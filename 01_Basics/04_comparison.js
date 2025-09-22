10c26be5db32bfec8d34caf727121c0ae32bb8ea
ayushmanalways <ayushmanalways0001@gmail.com>
2025-09-22 07:50:36 +0000

    datatypes_summary

--- 01_Basics/04_comparison.js
+++ 01_Basics/04_comparison.js
@@ -21,7 +21,7 @@ console.log(undefined < 0);
 console.log("2" === 2);
 
 
-// Stack- yaha pe data ka reference milta hai toh jo original value hoti hai usme koi change nhi hota ha tum reference leke secondary value me change kr skte ho
+// Stack- (For primitive datatypes) yaha pe data ka reference milta hai (copy value milta hai) toh jo original value hoti hai usme koi change nhi hota ha tum reference leke secondary value me change kr skte ho
 
-// Heap- ya pe actual data se reference milta hai yani variable direct data klo pin point karta hai agar ek me bhi change kiye toh dono change ho jayega
+// Heap- (For non-primitive datatypes) ya pe actual data se reference milta hai yani variable direct data klo pin point karta hai agar ek me bhi change kiye toh dono change ho jayega
 
